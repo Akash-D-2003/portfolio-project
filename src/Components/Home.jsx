44aec32819c1da66d0f251dcf60f46aa4a79fc41
Akash-D-2003 <akashking8589@gmail.com>
2024-09-30 19:13:59 +0530

    update resume

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import menImage from "../../src/images/men-image.png";
 import ringImage from "../../src/images/image-ring.png";
 import dIcon from "../../src/images/dwonload-icon.png";
+import cv from "../../public/fonts/Akash-d-resume.pdf";
 
 function Home() {
   return (
@@ -19,7 +20,7 @@ function Home() {
             </p>
           </div>
           <div className="col-lg-6 men-img">
-            <a href="../../public/fonts/Akash-d-resume.pdf" download className="cv-box">
+            <a href={cv} download className="cv-box">
               <div className="dwonload-box">
                 <div className="img">
                   <img src={dIcon} alt="icon" />
